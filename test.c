f8b56e951ab1f0939ca5bb2d9f74d081b2fda6ed
ccgargantua <lovelygargantua@gmail.com>
2023-09-29 01:11:17 -0400

    Cleaner macros for verbose failures

--- test.c
+++ test.c
@@ -14,6 +14,8 @@ static int total_tests = 0;
         fprintf(stderr, "Failed test '%s'\n", desc); \
 } while(0)
 
+#define TEST_EQUAL(a, b) do{TEST(a == b, #a " != " #b);}while(0)
+
 #define TEST_FATAL(exp, desc) TEST(exp, "FATAL: " desc); do{if(!(exp))abort();}while(0)
 
 static int temp_passed;
@@ -22,7 +24,7 @@ static int temp_total;
 #define REPORT(suite, name) do {                                                                             \
     temp_passed = passed_tests;                                                                              \
     temp_total = total_tests;                                                                                \
-    suite();                                                                                                   \
+    suite();                                                                                                 \
     fprintf(stderr, "Passed %d/%d tests in '%s'\n", passed_tests-temp_passed, total_tests-temp_total, name); \
 } while(0)
 
@@ -53,8 +55,8 @@ void test_arena_create(void)
     Arena *arena = arena_create(32);
     TEST_FATAL(arena != NULL, "Arena was NULL after creation. Fatal.");
     TEST_FATAL(arena->region != NULL, "Arena region was NULL");
-    TEST(arena->size == 32, "Arena struct member 'size' is incorrect.");
-    TEST(arena->index == 0, "Arena struct member 'index' is incorrect.");
+    TEST_EQUAL(arena->size, 32);
+    TEST_EQUAL(arena->index, 0);
     arena_destroy(arena);
 }
 
@@ -62,22 +64,36 @@ void test_arena_create(void)
 void test_arena_alloc(void)
 {
     Arena *arena = arena_create(13 + sizeof(long) * 3);
-    
+
     char *char_array = arena_alloc(arena, 13);
     TEST_FATAL(char_array != NULL, "char array allocated from arena was NULL.");
+
     memcpy(char_array, "Hello, world!", 13);
     TEST(memcmp(char_array, "Hello, world!", 13) == 0, "char array not copied correctly.");
-    TEST(arena->index == 13, "Arena index is incorrect after allocating for char array.");
+    TEST_EQUAL(char_array[0], 'H');
+    TEST_EQUAL(char_array[1], 'e');
+    TEST_EQUAL(char_array[2], 'l');
+    TEST_EQUAL(char_array[4], 'o');
+    TEST_EQUAL(char_array[7], 'w');
+    TEST_EQUAL(char_array[9], 'r');
+    TEST_EQUAL(char_array[10], 'l');
+    TEST_EQUAL(char_array[11], 'd');
+
+    TEST_EQUAL(arena->index, 13);
 
     long *long_array = arena_alloc(arena, sizeof(long) * 3);
     TEST_FATAL(long_array != NULL, "long array allocated from arena was NULL.");
+
     long expected[3] = {999, 9999, 99999};
     memcpy(long_array, expected, sizeof(long) * 3);
-    TEST(memcmp(long_array, expected, sizeof(long) * 3) == 0, "long array not copied correctly.");
+    TEST_EQUAL(long_array[0], 999);
+    TEST_EQUAL(long_array[1], 9999);
+    TEST_EQUAL(long_array[2], 99999);
 
-    TEST(arena->index == arena->size, "arena index does not match the size after being filled.");
+    TEST_EQUAL(arena->index, arena->size);
+
     char *should_not_be_allocated = arena_alloc(arena, 1);
-    TEST(should_not_be_allocated == NULL, "Arena allocation should should have failed after reaching capacity.");
+    TEST_EQUAL(should_not_be_allocated, NULL);
 
     arena_destroy(arena);
 }
@@ -86,16 +102,21 @@ void test_arena_alloc(void)
 void test_arena_alloc_aligned(void)
 {
     Arena *arena = arena_create(64);
+
     arena_alloc_aligned(arena, 8, 4);
-    TEST(arena->index == 8, "Arena incorrectly aligned after first allocation.");
+    TEST_EQUAL(arena->index, 8);
+
     arena_alloc_aligned(arena, 3, 4);
-    TEST(arena->index == 11, "Arena incorrectly aligned after second allocation.");
+    TEST_EQUAL(arena->index, 11);
+    
     arena_alloc_aligned(arena, 12, 4);
-    TEST(arena->index == 24, "Arena incorrectly aligned after third allocation.");
+    TEST_EQUAL(arena->index, 24);
+    
     arena_alloc_aligned(arena, 3, 4);
-    TEST(arena->index == 27, "Arena incorrectly aligned after fourth allocation.");
+    TEST_EQUAL(arena->index, 27);
+    
     arena_alloc_aligned(arena, 1, 4);
-    TEST(arena->index == 29, "Arena incorrectly aligned after fifth allocation.");
+    TEST_EQUAL(arena->index, 29);
 
     arena_destroy(arena);
 }
@@ -106,6 +127,6 @@ void test_arena_clear(void)
     Arena *arena = arena_create(10);
     arena->index = 5;
     arena_clear(arena);
-    TEST(arena->index == 0, "Arena index not 0 after being reset.");
+    TEST_EQUAL(arena->index, 0);
     arena_destroy(arena);
 }
